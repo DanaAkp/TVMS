354541d9e1fb574deea4116369cb7d5179e19c18
Danagul Akpaeva <aidanagul10@gmail.com>
2019-03-03 11:11:38 +0300

    допилить тестовый класс

--- Discriptive_statistics/DescriptiveStatistics.cs
+++ Discriptive_statistics/DescriptiveStatistics.cs
@@ -8,58 +8,239 @@ namespace Discriptive_statistics
 {
     class DescriptiveStatistics
     {
-        public static double Average(double[] array, int n)
+        public static double[] array { get; set; }
+
+        #region Описательная статистика
+        /// <summary>
+        /// Нахождение среднего для описательной статистики
+        /// </summary>
+        /// <returns></returns>
+        public static double Average()
         {
             double res = 0;
-            for (int i = 0; i < n; i++) res += array[i];
-            return res / n;
+            for (int i = 0; i < array.Length; i++) res += array[i];
+            return res / array.Length;
         }
-        public static double StandartError(double[] array, int n)
+        /// <summary>
+        /// Нахождение стандартной ошибки для описательной статистики
+        /// </summary>
+        /// <returns></returns>
+        public static double StandartError()
         {
             return 0;
         }
-        public static double Fashion(double[] array, int n)
+        /// <summary>
+        /// Нахождение моды для описательной статистики
+        /// </summary>
+        /// <returns></returns>
+        public static double Fashion()
+        {
+            Dictionary<double, int> dic = new Dictionary<double, int>();
+            for(int i = 0; i < array.Length; i++)
+            { int c = 0;
+                
+            }
+            return 0;
+        }
+        /// <summary>
+        /// Нахождение максимального элемента для описательной статистики
+        /// </summary>
+        /// <returns></returns>
+        public static double Max()
+        {
+            return array[array.Length - 1];
+        }
+        /// <summary>
+        /// Нахождение минимального элемента для описательной статистики
+        /// </summary>
+        /// <returns></returns>
+        public static double Min()
+        {
+            return array[0];
+        }
+        /// <summary>
+        /// Нахождение эксцеса для описательной статистики
+        /// </summary>
+        /// <returns></returns>
+        public static double Excess()
         {
             return 0;
         }
-        public static double Max(double[] array, int n)
-        {
-            double max = array[0];
-            for (int i = 1; i < n; i++)
-                if (array[i] > max) max = array[i];
-            return max;
-        }
-        public static double Min(double[] array, int n)
-        {
-            double min = array[0];
-            for (int i = 1; i < n; i++)
-                if (array[i] < min) min = array[i];
-            return min;
-        }
-        public static double Excess(double[] array, int n)
-        {
-            return 0;
-        }
-        public static double Amount(double[] array, int n)
+        /// <summary>
+        /// Нахождение суммы всех элементов для описательной статистики
+        /// </summary>
+        /// <returns></returns>
+        public static double Amount()
         {
             double sum = array[0];
-            for (int i = 1; i < n; i++)
+            for (int i = 1; i < array.Length; i++)
                 sum += array[i];
             return sum;
         }
-        public static double Interval(double[] array, int n)
+        /// <summary>
+        /// Нахождение интервала для описательной статистики
+        /// </summary>
+        /// <returns></returns>
+        public static double Interval()
         {
             return 0;
         }
-        public static double StandartDeviation(double[] array, int n)
+        /// <summary>
+        /// Нахождение стандартного отклонения для описательной статистики
+        /// </summary>
+        /// <returns></returns>
+        public static double StandartDeviation()
         {
             return 0;
         }
-        public static double Asymmetry(double[] array, int n)
+        /// <summary>
+        /// Нахождение асимметрии для описательной статистики
+        /// </summary>
+        /// <returns></returns>
+        public static double Asymmetry()
         {
             return 0;
         }
-        public static double Median(double[] array, int n)
+        /// <summary>
+        /// Нахождение медианы для описательной статистики
+        /// </summary>
+        /// <returns></returns>
+        public static double Median()
+        {
+            quicksort(array, 0, array.Length - 1);
+            if (array.Length % 2 != 0) return array.Length / 2 + 1;
+            return (array[array.Length / 2] + array[array.Length / 2 - 1]) / 2;
+        }
+        //public static double Score(double[] array, int n)
+        //{
+        //    return n;
+        //}
+        /// <summary>
+        /// Нахождение дисперсии для описательной статистики
+        /// </summary>
+        /// <returns></returns>
+        public static double Dispersion()
+        {
+            return 0;
+        }
+
+        public override string ToString()
+        {
+            string s = "Описательная статистика: ";
+            s += "\nСреднее: " + Average();
+            s += "\nСумма: " + Amount();
+            return s;
+        }
+        #endregion
+
+        #region Функции
+        /// <summary>
+        /// Нахождение среднего для какой-то совокупности
+        /// </summary>
+        /// <param name="arr">Какая-то совокупность</param>
+        /// <returns></returns>
+        public static double Average(double[] arr)
+        {
+            double res = 0;
+            for (int i = 0; i < array.Length; i++) res += array[i];
+            return res / array.Length;
+        }
+        /// <summary>
+        /// Нахождение стандартной ошибки для какой-то совокупности
+        /// </summary>
+        /// <param name="arr">Какая-то совокупность</param>
+        /// <returns></returns>
+        public static double StandartError(double[] arr)
+        {
+            return 0;
+        }
+        /// <summary>
+        /// Нахождение моды для какой-то совокупности
+        /// </summary>
+        /// <param name="arr">Какая-то совокупность</param>
+        /// <returns></returns>
+        public static double Fashion(double[] arr)
+        {
+            return 0;
+        }
+        /// <summary>
+        /// Нахождение максимального элемента для какой-то совокупности
+        /// </summary>
+        /// <param name="arr">Какая-то совокупность</param>
+        /// <returns></returns>
+        public static double Max(double[] arr)
+        {
+            double max = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+                if (arr[i] > max) max = arr[i];
+            return max;
+        }
+        /// <summary>
+        /// Нахождение минимального элемента для какой-то совокупности
+        /// </summary>
+        /// <param name="arr">Какая-то совокупность</param>
+        /// <returns></returns>
+        public static double Min(double[] arr)
+        {
+            double min = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+                if (arr[i] < min) min = arr[i];
+            return min;
+        }
+        /// <summary>
+        /// Нахождение эксцеса для какой-то совокупности
+        /// </summary>
+        /// <param name="arr">Какая-то совокупность</param>
+        /// <returns></returns>
+        public static double Excess(double[] arr)
+        {
+            return 0;
+        }
+        /// <summary>
+        /// Нахождение суммы всех элементов для какой-то совокупности
+        /// </summary>
+        /// <param name="arr">Какая-то совокупность</param>
+        /// <returns></returns>
+        public static double Amount(double[] arr)
+        {
+            double sum = array[0];
+            for (int i = 1; i < array.Length; i++)
+                sum += array[i];
+            return sum;
+        }
+        /// <summary>
+        /// Нахождение интервала для какой-то совокупности
+        /// </summary>
+        /// <param name="arr">Какая-то совокупность</param>
+        /// <returns></returns>
+        public static double Interval(double[] arr)
+        {
+            return 0;
+        }
+        /// <summary>
+        /// Нахождение стандартного отклонения для какой-то совокупности
+        /// </summary>
+        /// <param name="arr">Какая-то совокупность</param>
+        /// <returns></returns>
+        public static double StandartDeviation(double[] arr)
+        {
+            return 0;
+        }
+        /// <summary>
+        /// Нахождение асимметрии для какой-то совокупности
+        /// </summary>
+        /// <param name="arr">Какая-то совокупность</param>
+        /// <returns></returns>
+        public static double Asymmetry(double[] arr)
+        {
+            return 0;
+        }
+        /// <summary>
+        /// Нахождение медианы для какой-то совокупности
+        /// </summary>
+        /// <param name="arr">Какая-то совокупность</param>
+        /// <returns></returns>
+        public static double Median(double[] arr)
         {
 
             return 0;
@@ -68,11 +249,19 @@ namespace Discriptive_statistics
         //{
         //    return n;
         //}
-        public static double Dispersion(double[] array, int n)
+        /// <summary>
+        /// Нахождение дисперсии для какой-то совокупности
+        /// </summary>
+        /// <param name="arr">Какая-то совокупность</param>
+        /// <returns></returns>
+        public static double Dispersion(double[] arr)
         {
             return 0;
         }
-        public static int partition<T>(T[] m, int a, int b)  where T : IComparable<T>
+        #endregion
+
+        #region Сортировка
+        public static int partition<T>(T[] m, int a, int b) where T : IComparable<T>
         {
             int i = a;
             for (int j = a; j <= b; j++)         // просматриваем с a по b
@@ -94,12 +283,6 @@ namespace Discriptive_statistics
             quicksort(m, a, c - 1);
             quicksort(m, c + 1, b);
         }
-
-        public override string ToString()
-        {
-            string s = "Описательная статистика: ";
-            s += "\nСреднее: ";// + Average();
-            return base.ToString();
-        }
+        #endregion
     }
 }
---
+++ Discriptive_statistics/testLINQ.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discriptive_statistics
+{
+    class testLINQ
+    {
+        public static double[] array { get; set; }
+
+        #region Функции
+        /// <summary>
+        /// Нахождение среднего для какой-то совокупности
+        /// </summary>
+        /// <param name="arr">Какая-то совокупность</param>
+        /// <returns></returns>
+        public static double Average(double[] arr)
+        {
+            return array.Sum() / array.Length;
+        }
+        /// <summary>
+        /// Нахождение стандартной ошибки для какой-то совокупности
+        /// </summary>
+        /// <param name="arr">Какая-то совокупность</param>
+        /// <returns></returns>
+        public static double StandartError(double[] arr)
+        {
+            return 0;
+        }
+        /// <summary>
+        /// Нахождение моды для какой-то совокупности
+        /// </summary>
+        /// <param name="arr">Какая-то совокупность</param>
+        /// <returns></returns>
+        public static double Fashion(double[] arr)
+        {
+            var dic = array.GroupBy(x => x).Where(g => g.Count() > 1).Select(y => new { Element = y.Key, Count = y.Count() }).ToList();
+            var w = from v in dic orderby v.Count select v;
+            return w.Last().Element;
+        }
+        /// <summary>
+        /// Нахождение максимального элемента для какой-то совокупности
+        /// </summary>
+        /// <param name="arr">Какая-то совокупность</param>
+        /// <returns></returns>
+        public static double Max(double[] arr)
+        {
+            return array.Max();
+        }
+        /// <summary>
+        /// Нахождение минимального элемента для какой-то совокупности
+        /// </summary>
+        /// <param name="arr">Какая-то совокупность</param>
+        /// <returns></returns>
+        public static double Min(double[] arr)
+        {
+            return array.Min();
+        }
+        /// <summary>
+        /// Нахождение эксцеса для какой-то совокупности
+        /// </summary>
+        /// <param name="arr">Какая-то совокупность</param>
+        /// <returns></returns>
+        public static double Excess(double[] arr)
+        {
+            return 0;
+        }
+        /// <summary>
+        /// Нахождение суммы всех элементов для какой-то совокупности
+        /// </summary>
+        /// <param name="arr">Какая-то совокупность</param>
+        /// <returns></returns>
+        public static double Amount(double[] arr)
+        {
+            return array.Sum();
+        }
+        /// <summary>
+        /// Нахождение интервала для какой-то совокупности
+        /// </summary>
+        /// <param name="arr">Какая-то совокупность</param>
+        /// <returns></returns>
+        public static double Interval(double[] arr)
+        {
+            return 0;
+        }
+        /// <summary>
+        /// Нахождение стандартного отклонения для какой-то совокупности
+        /// </summary>
+        /// <param name="arr">Какая-то совокупность</param>
+        /// <returns></returns>
+        public static double StandartDeviation(double[] arr)
+        {
+            return 0;
+        }
+        /// <summary>
+        /// Нахождение асимметрии для какой-то совокупности
+        /// </summary>
+        /// <param name="arr">Какая-то совокупность</param>
+        /// <returns></returns>
+        public static double Asymmetry(double[] arr)
+        {
+            return 0;
+        }
+        /// <summary>
+        /// Нахождение медианы для какой-то совокупности
+        /// </summary>
+        /// <param name="arr">Какая-то совокупность</param>
+        /// <returns></returns>
+        public static object Median(double[] arr)
+        {
+            var s = from t in array orderby t select t;
+             var d=s.Select ((elem, index) => new { index, elem = array[index] });
+            //if(s.ToArray().Count()%2!=0)
+                return s.ToArray().GetValue(s.ToArray().Count() / 2 + 1);
+            //return (s.ToArray().GetValue(s.ToArray().Count() / 2) + s.ToArray().GetValue(s.ToArray().Count() / 2 - 1)) / 2;
+
+           // return 0;
+        }
+        //public static double Score(double[] array, int n)
+        //{
+        //    return n;
+        //}
+        /// <summary>
+        /// Нахождение дисперсии для какой-то совокупности
+        /// </summary>
+        /// <param name="arr">Какая-то совокупность</param>
+        /// <returns></returns>
+        public static double Dispersion(double[] arr)
+        {
+
+            return 0;
+        }
+        #endregion
+    }
+}
